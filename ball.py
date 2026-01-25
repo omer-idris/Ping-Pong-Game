369c61692c4c7e243640b2f3e6088fb1702ea499
OMER IDRIS <omabrm2000@gmail.com>
2026-01-26 01:35:30 +0300

    Update ball.py

--- ball.py
+++ ball.py
@@ -1,2 +1,21 @@
 from turtle import Turtle, Screen
 import time, os, random
+
+class Ball(Turtle):
+  def __init__(self):
+    super().__init__()
+         self.shape('circle')
+         self.color('white')
+         self.penup()
+         self.x_move = 10
+         self.y_move = 10
+        
+        
+class Scorer(Turtle):
+        def __init__(self, position_given, shape = "classic", undobuffersize = 1000, visible = False):
+             super().__init__(shape, undobuffersize, visible)
+             self.penup()
+             self.goto(position_given)
+             self.score_left = 0
+             self.score_right = 0
+    
