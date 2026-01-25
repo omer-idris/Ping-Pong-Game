e536026cac2dc523dd57c7ad3020e991ce171674
OMER IDRIS <omabrm2000@gmail.com>
2026-01-26 01:39:19 +0300

    Create paddles.py

---
+++ paddles.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen
+import time, os, random
+
+class Paddle(Turtle):
+    def __init__(self, position__to, color):
+        super().__init__()
+        self.shape('square')
+        self.penup()
+        self.color(color)
+        self.goto(position__to)
+        self.shapesize(stretch_len=1, stretch_wid=5)
+    
+    def go_up(self):
+        self.goto(self.xcor(), self.ycor()+100)
+
+    def go_down(self):
+        self.goto(self.xcor(), self.ycor()-100)
