99224b421c1984ba6f3e38d708a4acdde2f9c7a8
OMER IDRIS <omabrm2000@gmail.com>
2026-01-28 10:52:38 +0300

    Update ball.py

--- ball.py
+++ ball.py
@@ -1,21 +1,38 @@
 from turtle import Turtle, Screen
-import time, os, random
+import random, os, time
 
 class Ball(Turtle):
-  def __init__(self):
-    super().__init__()
-         self.shape('circle')
-         self.color('white')
-         self.penup()
-         self.x_move = 10
-         self.y_move = 10
-        
-        
+    def __init__(self):
+        super().__init__()
+        self.shape('circle')
+        self.color('white')
+        self.penup()
+        self.x_move = 10
+        self.y_move = 10
+
 class Scorer(Turtle):
-        def __init__(self, position_given, shape = "classic", undobuffersize = 1000, visible = False):
-             super().__init__(shape, undobuffersize, visible)
-             self.penup()
-             self.goto(position_given)
-             self.score_left = 0
-             self.score_right = 0
+    def __init__(self, color, position_assigned):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.color(color)
+        self.goto(position_assigned)
+        self.r_point = 0
+        self.l_point = 0
     
+    def add_score(self, determiner):
+        self.clear()
+        if determiner == 'left':
+            self.l_point += 1
+            self.write(f"{self.l_point}", font=('arial', 29, 'normal'))
+        if determiner == 'right':
+            self.r_point += 1
+            self.write(f"{self.r_point}", font=('arial', 29, 'normal'))
+        
+
+
+
+
+
+
+
