d17b854645cf2f0dc2d55b5b40901e8414c0b282
OMER IDRIS <omabrm2000@gmail.com>
2026-01-28 10:51:35 +0300

    Update paddles.py

--- paddles.py
+++ paddles.py
@@ -1,26 +1,18 @@
+import random, os, time
 from turtle import Turtle, Screen
-import time, os, random
+
 
 class Paddle(Turtle):
-    def __init__(self, position__to, color):
-        super().__init__()
-        self.shape('square')
+    def __init__(self, position_to, color, shape='square'):
+        super().__init__(shape)
         self.penup()
+        self.shapesize(stretch_len=1,stretch_wid=5)
+        self.goto(position_to)
         self.color(color)
-        self.goto(position__to)
-        self.shapesize(stretch_len=1, stretch_wid=5)
+        self.speed('fastest')
     
     def go_up(self):
         self.goto(self.xcor(), self.ycor()+100)
 
     def go_down(self):
         self.goto(self.xcor(), self.ycor()-100)
-
-class Drawer(Turtle):
-    def __init__(self):
-        super().__init__()
-        self.color('orange')
-        self.penup()
-        self.hideturtle()
-        self.pensize(10)
-        self.speed('fastest')
